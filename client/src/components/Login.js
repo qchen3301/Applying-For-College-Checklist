46e867c80762f019f821b04243b7a4c8c273789b
qchen3301 <42414585+qchen3301@users.noreply.github.com>
2018-10-09 15:00:31 -0400

    hell yeah inputs working

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -14,18 +14,20 @@ export default class Login extends Component {
     }
   }
 
+  //call axios directly in the compoDidMount function
   componentDidMount = async () => {
     const response = await axios.get('/api/students')
     this.setState({students: response.data})
   }
 
   handleChange = (event) => {
+    console.log('changing', event.target.name, event.target.value)
     //take it
     const newStudent = {...this.state.newStudent}
     //change it???????
-    newStudent[event.target.adduser] = event.target.value
+    newStudent[event.target.name] = event.target.value
     //thats it boi...get in thar nice and DEEP like
-    this.setState({newStudent})
+    this.setState({ newStudent })
   }
 
   handleSubmit = async (event) => {
@@ -55,32 +57,32 @@ export default class Login extends Component {
         <form onSubmit={this.handleSubmit}>
           <input
           type='text'
-          name='adduser'
+          name='username'
           value={this.state.newStudent.username}
           placeholder = 'enter a username'
           onChange={this.handleChange} />
           <input 
           type = 'text'
-          name = 'adduser'
+          name = 'grade'
           value = {this.state.newStudent.grade}
           placeholder = 'enter your grade (9-12)'
           maxLength = '6'
           onChange = {this.handleChange} />
           <input
           type = 'text'
-          name = 'adduser'
+          name = 'lastName'
           value = {this.state.newStudent.lastName}
           placeholder = 'enter your family name'
           onChange = {this.handleChange} />
           <input 
           type = 'text'
-          name = 'adduser'
+          name = 'firstName'
           value = {this.state.newStudent.firstName}
           placeholder = 'enter your given name'
           onChange = {this.handleChange} />
           <input 
           type = 'text'
-          name = 'adduser'
+          name = 'highSchool'
           value = {this.state.newStudent.highSchool}
           placeholder = 'enter your high school'
           onChange = {this.handleChange} />
