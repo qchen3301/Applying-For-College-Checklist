feec9866c1b28d9bb29715b9eecac7350aa864bb
qchen3301 <42414585+qchen3301@users.noreply.github.com>
2018-10-11 21:44:22 -0400

    added assets

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
-import Paper from '@material-ui/core'
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-
+      <StyledDiv>
+        <img src="" alt="COLLEGE"/>
         <Link to='/login'>Continue</Link>
-      </div>
+      </StyledDiv>
     )
   }
 }
