810d0ba05c9cb33ddc21428a49b15acc8046c421
qchen3301 <42414585+qchen3301@users.noreply.github.com>
2018-10-12 02:56:00 -0400

    working?? reseeded db after restarted localmachine

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+// when u see the doge u see the code...
 import React, { Component } from 'react'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from './components/Home'
